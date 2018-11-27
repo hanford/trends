cfa28e8648b986b3deeccc02b711d6eba8f99b53
Jack Hanford <jackhanford@gmail.com>
2018-11-27 11:03:05 -0800

    disable next-offline

--- www/next.config.js
+++ www/next.config.js
@@ -2,31 +2,32 @@ const withOffline = require('next-offline')
 
 const isDev = process.env.NODE_ENV !== 'production'
 
-console.log(process.env.NOW_URL)
-
-module.exports = withOffline({
+module.exports = {
   publicRuntimeConfig: {
     api: isDev ? 'http://localhost:2999' : typeof window !== 'undefined' ? '' : process.env.NOW_URL,
     googleAnalytics: isDev ? '' : 'UA-45226320-5',
     isDev,
   },
-  workboxOpts: {
-    runtimeCaching: [
-      {
-        urlPattern: /^https?.*/,
-        handler: 'networkFirst',
-        options: {
-          cacheName: 'https-calls',
-          networkTimeoutSeconds: 15,
-          expiration: {
-            maxEntries: 150,
-            maxAgeSeconds: 30 * 24 * 60 * 60
-          },
-          cacheableResponse: {
-            statuses: [0, 200]
-          }
-        }
-      }
-    ]
-  }
-})
+}
+
+// module.exports = withOffline({
+//   workboxOpts: {
+//     runtimeCaching: [
+//       {
+//         urlPattern: /^https?.*/,
+//         handler: 'networkFirst',
+//         options: {
+//           cacheName: 'https-calls',
+//           networkTimeoutSeconds: 15,
+//           expiration: {
+//             maxEntries: 150,
+//             maxAgeSeconds: 30 * 24 * 60 * 60
+//           },
+//           cacheableResponse: {
+//             statuses: [0, 200]
+//           }
+//         }
+//       }
+//     ]
+//   }
+// })
