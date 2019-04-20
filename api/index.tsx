fbe4048a1ec0a1a9142309a541e8652caa9c7256
Jack Hanford <jackhanford@gmail.com>
2019-04-20 13:01:08 -0700

    add s-maxage

--- api/index.tsx
+++ api/index.tsx
@@ -4,19 +4,12 @@ import express from "express";
 import schema from "./graphql";
 
 const port = parseInt(process.env.PORT || "2999", 10) || 2999;
-const dev = process.env.NODE_ENV !== "production";
 
 const server = new ApolloServer({ schema, introspection: true });
 
 const app = express();
-server.applyMiddleware({ app, path: "/api/graphql", cors: true });
 
-if (!dev) {
-  app.get("*", (_, res, next) => {
-    res.setHeader("Cache-Control", "max-age=43200, immutable");
-    next();
-  });
-}
+server.applyMiddleware({ app, path: "/api/graphql", cors: true });
 
 app.get("/", (_req, res) => res.redirect("/api/graphql"));
 
